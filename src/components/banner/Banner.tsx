94b394a9c8a110e99cbc1ccc849994d01c7041f7
badrulalambd <badrulalam.ju@gmail.com>
2026-09-12 21:52:27 +0600

    Header and Banner section is fully completed according to the project requirements. And I have also made the sections responsive for all the devics

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -5,22 +5,32 @@ export default function Banner() {
 
     return (
         <>
-            <div className="bg-yellow-100">
-                <section className="container mx-auto py-10 flex justify-between items-center gap-5">
-                    <div>
-                        <div>
-                            <h1>Build Your Ideal Development Stack</h1>
-                            <p>Explore frontend, backend, database, and tooling options,
+            <div className="">
+                <section className="p-5 container mx-auto py-10 flex flex-col lg:flex-row justify-between  gap-0 lg:gap-5 mt-30 lg:mt-20">
+                    <div className="flex-1 flex flex-col justify-center gap-8">
+                        <div className="flex flex-col gap-5">
+                            <h1 className="text-gray-800 text-center lg:text-start text-5xl lg:text-7xl font-extrabold">Build Your Ideal<br />
+                                <span className="bg-linear-to-r from-[#FF5722] via-[#D81B7E] to-[#7C3AED] bg-clip-text text-transparent">Development Stack</span></h1>
+                            <p className="text-center lg:text-start text-[20px] lg:text-[25px] text-gray-500">Explore frontend, backend, database, and tooling options,
                                 compare them side by side, and put together the stack that fits your
                                 next project.</p>
                         </div>
-                        <div>
-                            <button className="btn btn-active btn-secondary">Secondary</button>
-                            <button className="btn btn-outline btn-secondary">Secondary</button>
+                        <div className="flex flex-wrap justify-center lg:justify-start gap-3 mt-3">
+                            <button
+                                className="btn border-0 bg-linear-to-r from-[#F97316] to-[#EC4899] px-7 py-3 text-white font-semibold rounded-xl shadow-md shadow-pink-200/40 transition-all duration-300 hover:-translate-y-0.5 hover:shadow-lg hover:shadow-pink-200/50"
+                            >
+                                Explore Technologies
+                            </button>
+
+                            <button
+                                className="btn bg-white px-7 py-3 text-gray-700 font-semibold rounded-xl border border-gray-200 shadow-sm transition-all duration-300 hover:-translate-y-0.5 hover:border-gray-300 hover:shadow-md"
+                            >
+                                Learn More
+                            </button>
                         </div>
                     </div>
-                    <div>
-                        <img src={stack} alt="banner image" />
+                    <div className="flex-1 flex justify-center items-center]">
+                        <img className="-my-10 lg:my-0" src={stack} alt="banner image" />
                     </div>
                 </section>
             </div>
--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,32 +1,43 @@
 
+import { useState } from "react"
 import logo from "../../assets/logo-text.png"
+import hamburger from "../../assets/hamburger.png"
 
 export default function NavBar() {
 
+    const [activeMenu, setActiveMenu] = useState("Home");
+
     return (
         <>
-            <div>
+            <div className="fixed top-0 left-0 right-0 bg-white border-b border-gray-100 z-999">
                 <header className="container mx-auto flex justify-between items-center gap-5 p-5">
+                    <div className="lg:hidden ">
+                        <img src={hamburger} alt="Hamburger" />
+                    </div>
                     <div>
                         <img className="cursor-pointer" src={logo} alt="Logo" />
                     </div>
 
-                    <nav>
+                    <nav className="hidden lg:block">
                         <ul className="flex gap-5">
-                            <li className="text-gray-600 font-semibold cursor-pointer">Home</li>
-                            <li className="text-gray-600 font-semibold cursor-pointer">Technologies</li>
-                            <li className="text-gray-600 font-semibold cursor-pointer">Projects</li>
-                            <li className="text-gray-600 font-semibold cursor-pointer">About</li>
-                            <li className="text-gray-600 font-semibold cursor-pointer">Contact</li>
+                            <li onClick={()=>setActiveMenu("Home")}
+                            className={activeMenu==="Home" ? "text-[#D91B7E] font-semibold cursor-pointer" : "text-gray-500 font-semibold cursor-pointer"}>Home</li>
+                            <li onClick={()=>setActiveMenu("Technologies")}
+                            className={activeMenu==="Technologies" ? "text-[#D91B7E] font-semibold cursor-pointer" : "text-gray-500 font-semibold cursor-pointer"}>Technologies</li>
+                            <li onClick={()=>setActiveMenu("Projects")}
+                            className={activeMenu==="Projects" ? "text-[#D91B7E] font-semibold cursor-pointer" : "text-gray-500 font-semibold cursor-pointer"}>Projects</li>
+                            <li onClick={()=>setActiveMenu("About")}
+                            className={activeMenu==="About" ? "text-[#D91B7E] font-semibold cursor-pointer" : "text-gray-500 font-semibold cursor-pointer"}>About</li>
+                            <li onClick={()=>setActiveMenu("Contact")}
+                            className={activeMenu==="Contact" ? "text-[#D91B7E] font-semibold cursor-pointer" : "text-gray-500 font-semibold cursor-pointer"}>Contact</li>
                         </ul>
                     </nav>
 
                     <div className="flex gap-5">
-                        <button className="btn-ghost text-gray-600 font-semibold cursor-pointer">Sign In</button>
+                        <button className="btn-ghost text-gray-500 font-semibold cursor-pointer">Sign In</button>
                         <button className="btn btn-secondary rounded-4xl px-6 py-2 text-white font-semibold cursor-pointer">Sign Up</button>
                     </div>
                 </header>
-                <hr className="text-gray-100" />
             </div>
         </>
     )
